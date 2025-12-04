d458eb9466d3739ee81f708954f8549dadc01c22
giihdot <giihferreira0708@gmail.com>
2025-12-04 07:56:22 -0300

    footer arrumado

--- projetoAPI/src/Components/Footer.jsx
+++ projetoAPI/src/Components/Footer.jsx
@@ -5,30 +5,40 @@ import { Link } from "react-router-dom";
 export default function Footer() {
   return (
     <footer className="footer">
-      <div className="footer-section">
-        <h4>Guia Crescer e Aprender</h4>
-        <p>Transformando a educação através do afeto, técnica e colaboração.</p>
-      </div>
+      <div className="footer-content">
 
-      <div className="footer-section">
-        <h4>Links Rápidos</h4>
-        <ul>
-          <li>
-          <Link to="/calendario">
-            <i></i> Calendário Pedagógico
-          </Link>
-        </li>
-        </ul>
-      </div>
+        <div className="footer-section">
+          <h4>Guia Crescer e Aprender</h4>
+          <p>Transformando a educação através do afeto, tecnologia e colaboração.</p>
+        </div>
 
-      <div className="footer-section">
-        <h4>Colaboradores</h4>
-        <ul>
-          <li>Giovanna Ferreira</li>
-          <li>Julisa Piazzoli</li>
-          <li>Kemilly Barra</li>
-          <li>Maria Duarte</li>
+        <div className="footer-section">
+          <h4>Links Rápidos</h4>
+          <ul>
+            <li><Link to="/calendario">Calendário Escolar</Link></li>
+            <li><Link to="/ia">IA Pedagógica</Link></li>
+            <li><Link to="/asgatitas">Colaboradores</Link></li>
+          </ul>
+        </div>
+
+        <div className="footer-section">
+      
+          <h4>Colaboradoras:</h4>
+         <ul>
+            <li>Giovanna Ferreira</li>
+            <li>Julia Piazzoli</li>
+            <li>Kamilly Barra</li>
+            <li>Maria Clara Duarte</li>
         </ul>
+        </div>
+
+      </div>
+      
+      <div className="footer-copy">
+        <br/> 
+        <br/>
+        <br/>
+        © 2024 Guia Crescer e Aprender. Todos os direitos reservados.
       </div>
     </footer>
   );
